bf8cec9f21effd7316b5d4153fb03cf06020b72a
lkdoe <leonhard.doerflinger@gmail.com>
2023-12-18 23:37:16 +0100

    added primefactor function and infinite list of primes

--- lab.hs
+++ lab.hs
@@ -3,3 +3,15 @@ isittrue x = if x
     else "No"
 
 isitreallytrue x = if x then "Really yes" else "Really no"
+
+primes = sieve [2..]
+    where
+        sieve (p:xs) = p : sieve [x | x <- xs, mod x p > 0]
+
+primefactors n = factor n primes
+    where
+        factor n (p:ps)
+            | n < 2         = []
+            | p * p > n     = [n]
+            | mod n p == 0  = p : factor (div n p) (p:ps)
+            | otherwise     = factor n ps
