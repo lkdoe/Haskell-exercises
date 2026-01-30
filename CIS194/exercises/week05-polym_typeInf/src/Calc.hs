f5e97c90da3341b957a2d7f78a517a1b2f383d3f
Leo <leonhard.doerflinger@gmail.com>
2026-01-30 18:28:09 +0100

    found some kind of solution for Exercise 5

--- CIS194/exercises/week05-polym_typeInf/src/Calc.hs
+++ CIS194/exercises/week05-polym_typeInf/src/Calc.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Calc where
 
 import ExprT
 import Parser (parseExp)
+import StackVM
 
 -- # Exercise 1
 eval :: ExprT -> Integer
 eval expr = case expr of
-    (Lit i) -> i
-    (Add e1 e2) -> eval e1 + eval e2
-    (Mul e1 e2) -> eval e1 * eval e2
+    (ExprT.Lit i) -> i
+    (ExprT.Add e1 e2) -> eval e1 + eval e2
+    (ExprT.Mul e1 e2) -> eval e1 * eval e2
 
 -- # Exercise 2
 -- evalStr :: String-> Maybe Integer
@@ -18,7 +21,7 @@ eval expr = case expr of
 -- Then I made the connection to the signature of evalStr and it clicked.
 
 evalStr :: String -> Maybe Integer
-evalStr exp = eval <$> parseExp Lit Add Mul exp
+evalStr exp = eval <$> parseExp ExprT.Lit ExprT.Add ExprT.Mul exp
 
 -- # Exercise 3
 
@@ -27,15 +30,21 @@ class Expr e where
     add :: e -> e -> e
     mul :: e -> e -> e
 
--- "ExprT." is optional but usefull for clarification.
+-- "ExprT." is optional if only the Expr module is imported.
+-- It is necessary when StackVM is imported also,
+-- because both modules have Add and Mul as constructors.
 instance Expr ExprT where
     lit = ExprT.Lit
-    add = Add
-    mul = Mul
+    add = ExprT.Add
+    mul = ExprT.Mul
 
 -- Given as an example for constraining the type of arguments:
 reify :: ExprT -> ExprT
 reify = id
+
+-- For Exercise 5:
+programify :: Program -> Program
+programify = id
 
 -- # Exercise 4
 -- The definition of the infix operators could also be done in this way:
@@ -73,3 +82,19 @@ testInteger = testExp :: Maybe Integer
 testBool = testExp :: Maybe Bool
 testMM = testExp :: Maybe MinMax
 testSat = testExp :: Maybe Mod7
+
+-- # Exercise 5
+-- Use the simulated VM from the provided StackVM.hs.
+-- Create an instance of Expr for the type StackVM.Program.
+-- Then create a compile function that creates a Program from a String.
+instance Expr StackVM.Program where
+    lit n = [StackVM.PushI n]
+    add n1 n2 = n1 ++ n2 ++ [StackVM.Add]
+    mul n1 n2 = n1 ++ n2 ++ [StackVM.Mul]
+
+-- Reminder:
+-- evalStr :: String -> Maybe Integer
+-- evalStr exp = eval <$> parseExp ExprT.Lit ExprT.Add ExprT.Mul exp
+
+compile :: String -> Maybe Program
+compile ex1 = programify <$> parseExp lit add mul ex1
